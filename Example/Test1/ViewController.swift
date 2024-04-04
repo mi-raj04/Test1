fe05d28263e311f937db199dd63b43c8bc1c0916
raj saija <raj.saija@mindinventory.com>
2024-04-04 11:02:28 +0530

    - first init

---
+++ Example/Test1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Test1
+//
+//  Created by raj saija on 04/04/2024.
+//  Copyright (c) 2024 raj saija. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
